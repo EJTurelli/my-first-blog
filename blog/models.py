6aa3d3c27b0eee2afd582ed5d338ab98b017f9cb
EJTurelli <ejturelli@yahoo.com>
2016-05-14 19:05:16 -0300

    Homework: create comment model

--- blog/models.py
+++ blog/models.py
@@ -14,6 +14,12 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
+    def approved_comments(self):
+    	return self.comments.filter(approved_comment=True)    
+
+    def not_approved_comments(self):
+    	return self.comments.filter(approved_comment=False)    
+
     def __str__(self):
         return self.title
 
